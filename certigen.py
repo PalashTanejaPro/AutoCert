5d5723f7c09c03f0aaf24dafdeb320e89be9bded
PalashTanejaPro <tanejapalash@gmail.com>
2017-07-29 20:56:46 +0530

    Added main script
    lol

---
+++ certigen.py
@@ -0,0 +1,32 @@
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+from flask import Flask, request
+
+img = Image.open("certitemplate.jpg")
+draw = ImageDraw.Draw(img)
+app = Flask(__name__)
+Name = ""
+School = ""
+event=""
+
+@app.route('/data')
+def data():
+	Name = request.args.get('name')
+	School = request.args.get('school')
+	event = request.args.get('event')
+	print Name
+	print School
+	print event
+	selectFont = ImageFont.truetype("Font.ttf", size = 70)
+	draw.text( (1050,1500), Name, (0,0,0), font=selectFont )
+	draw.text( (1050,1830), School, (0,0,0), font=selectFont )
+	draw.text( (1050,2170), event, (0,0,0), font=selectFont )
+	img.save( 'certi.pdf', "PDF", resolution=100.0)
+	return "Request Successful"
+#Name="Palash Taneja"
+#School = "DPS Rohini"
+#event = "Programming"
+
+app.run()
+
